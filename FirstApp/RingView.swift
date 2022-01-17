cb2eddc82055c83b8ec78d01e26ee6b7c221c70f
gmeyer <guumeyer@gmail.com>
2022-01-17 10:44:40 -0800

    Add Ring progression View

---
+++ FirstApp/RingView.swift
@@ -0,0 +1,20 @@
+//
+//  RingView.swift
+//  FirstApp
+//
+//  Created by meyer on 2022-01-17.
+//
+
+import SwiftUI
+
+struct RingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RingView_Previews: PreviewProvider {
+    static var previews: some View {
+        RingView()
+    }
+}
